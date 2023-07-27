018a034a3f66496838f756bab5f125c1129ebbd2
FlutterDevASMR <flutterdevasmr@gmail.com>
2023-07-28 00:11:53 +0330

    second commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ConstantsColors {
+  static Color greenColor = Color(0xff46d7d2);
+  static Color navBarBgColor = Color(0xff07b0b5);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallet_app_ui/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
